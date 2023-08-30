0dcac5083dcd09cdd8bd551075cc303d51a777c9
BTH-M3ND3S <mendessilveriomateus@gmail.com>
2023-08-30 16:45:24 -0300

    projeto finalizado

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { useEffect, useState } from 'react';
+import "./App.css"
+
 
 function App() {
 
     const [ listatarefas, setlistaTarefas] = useState([]);
     const [ tarefa, setTarefa ] = useState({id: '', texto:"", status: ""});
 
-    function addTarefa(){
+    function addTarefa(event){
 
-        if( tarefa.texto !== "")
+        if( tarefa.texto !== ""  && ( event.key == "Enter" || event == "click" ) )
         setlistaTarefas([...listatarefas, tarefa]);
 
     }
@@ -26,22 +28,26 @@ function App() {
         setTarefa({id:"", texto:"", status: "não concluida"});
       }, [listatarefas])
   return (
-    <>
+    <div className='caixagigante'>
     <header>
-      <h1>React DO</h1>
+        <h1 className='h1'><i class="fa-solid fa-mobile-screen-button"></i>Lista de Tarefas</h1>
     </header>
+    <div className='container'>
+      <div className='divizona'>
       <div>
-        <input onChange={(e) => setTarefa({id: Math.random(), texto: e.target.value, status: false})} type='text' name='tarefa' value={tarefa.texto} placeholder='Digite sua tarefa'/>
-        <button  onClick={addTarefa}>Adicionar</button>
+        <input className='inputadicionar' onChange={(e) => setTarefa({id: Math.random(), texto: e.target.value, status: false})} type='text' name='tarefa' value={tarefa.texto} placeholder='Digite sua tarefa' onKeyDown={ (e) => addTarefa(e) }/>
+        <button className='botaoadicionar' onClick={ (e) => addTarefa( "click" ) }><i class="fa-solid fa-plus fa-beat"></i></button>
       </div>
         <div>
           <ul>
             {listatarefas.map( (item, index) => (
-              <li key={index}>{item.texto} <button onClick={() => statustarefa(item.id, item.status)}>{item.status ? 'concluida' : 'não concluida'}</button> <button onClick={() => excluirtarefa(item.id)}>Excluir</button></li>
+              <li className={item.status ? 'nãoconcluida' :'concluida' } key={index}>{item.texto} <button className={item.status ? 'botaonãoconcluida' : 'botaoconcluida'} onClick={() => statustarefa(item.id, item.status)}>{item.status ?<i class="fa-solid fa-x fa-fade"></i> :<i class="fa-solid fa-check fa-fade" ></i> }</button> <button className={item.status ? 'boataoexcluirtrue' : 'botaoexcluirfalse'} onClick={() => excluirtarefa(item.id)}><i class="fa-solid fa-trash fa-bounce"></i></button></li>
             ))}
           </ul>
         </div>
-    </>
+      </div>
+      </div>
+    </div>
   );
 }
 export default App;
